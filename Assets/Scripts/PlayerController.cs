14615cad23ea6c7d7bdde0c8721611a7092f8697
William Barnes <jakebarnes1989@gmail.com>
2021-02-06 20:45:05 -0500

    Update PlayerController
    - Added ability to jump using AddForce
    - Setup gravity multiplayer to increase fall speed
    - Added ForceMode.Impulse to apply force immediatly

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody playerRB;
+    private int jumpPow = 1250;
+    private float gravityPow = 2.75f;
+    // private bool onGround = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRB = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityPow;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            PlayerJump();
+        }
+    }
+
+    void PlayerJump()
+    {
+        playerRB.AddForce(Vector3.up * jumpPow, ForceMode.Impulse);
     }
 }
