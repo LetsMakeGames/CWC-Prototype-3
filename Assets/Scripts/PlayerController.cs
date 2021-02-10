c1b4a514d612694ec0439cbc0b128402b7db6aee
William Barnes <jakebarnes1989@gmail.com>
2021-02-10 16:12:04 -0500

    Animation
    - Implemented player animations
    - Implemented gameOver check for player animations.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,22 +8,30 @@ public class PlayerController : MonoBehaviour
     private int jumpPow = 1250;
     private float gravityPow = 2.75f;
     private bool isOnGround = true;
+    private Animator playerAnim;
+
     public bool gameOver = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Get the RB and Animator component of the player
         playerRB = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
+
+        // Set the game gravity power.
         Physics.gravity *= gravityPow;
+
         gameOver = false;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
 
-
-        if (Input.GetButtonDown("Jump") && isOnGround)
+        // If jump button, player jump
+        if (Input.GetButtonDown("Jump") && isOnGround && !gameOver)
         {
             PlayerJump();
         }
@@ -31,19 +39,31 @@ public class PlayerController : MonoBehaviour
 
     void PlayerJump()
     {
+        // Set animator trigger for jump animation transition
+        playerAnim.SetTrigger("Jump_trig");
+
+        // User AddForce to jump using physics vs translation.
         playerRB.AddForce(Vector3.up * jumpPow, ForceMode.Impulse);
+
         isOnGround = false;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        // If check collision type
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
         } else if (collision.gameObject.CompareTag("Obstacle"))
         {
-            Debug.Log("Game Over!");
+            // Set player animator variables for transitioning to death animation
+            playerAnim.SetFloat("Speed_f", 0.0f);
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+
             gameOver = true;
+            Debug.Log("Game Over!");
+            
         }        
         
     }
