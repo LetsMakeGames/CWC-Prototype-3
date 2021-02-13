bb7ef137ed3c0c7f0fe0f7cf430a68c4b5055056
William Barnes <jakebarnes1989@gmail.com>
2021-02-12 22:12:36 -0500

    Particles and Audio (#1)
    * Particles and Audio
    
    - Implemented game SFX
    - Implemented footstep particles
    - Audio and particles now stop on Game Over.
    
    * Audio Update
    
    - Balancing
    - Implemented jump audio

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,8 +9,17 @@ public class PlayerController : MonoBehaviour
     private float gravityPow = 2.75f;
     private bool isOnGround = true;
     private Animator playerAnim;
+    private float counter;
 
     public bool gameOver = false;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioSource footStep;
+    public AudioSource impact;
+    public AudioSource deathSound;
+    public AudioSource jumpSound;
+
+    private AudioSource bgmAudioSource;
 
     // Start is called before the first frame update
     void Start()
@@ -23,17 +32,27 @@ public class PlayerController : MonoBehaviour
         Physics.gravity *= gravityPow;
 
         gameOver = false;
-        
+
+        bgmAudioSource = GameObject.Find("Main Camera").GetComponent<AudioSource>();
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        counter += Time.deltaTime;
 
         // If jump button, player jump
         if (Input.GetButtonDown("Jump") && isOnGround && !gameOver)
         {
             PlayerJump();
+        } 
+        
+        if (counter >= 0.25 && isOnGround && !gameOver)
+        {
+            footStep.Play();
+            counter = 0;
         }
     }
 
@@ -41,6 +60,8 @@ public class PlayerController : MonoBehaviour
     {
         // Set animator trigger for jump animation transition
         playerAnim.SetTrigger("Jump_trig");
+        dirtParticle.Stop();
+        jumpSound.Play();
 
         // User AddForce to jump using physics vs translation.
         playerRB.AddForce(Vector3.up * jumpPow, ForceMode.Impulse);
@@ -54,12 +75,18 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            dirtParticle.Play();
         } else if (collision.gameObject.CompareTag("Obstacle"))
         {
             // Set player animator variables for transitioning to death animation
             playerAnim.SetFloat("Speed_f", 0.0f);
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            bgmAudioSource.Stop();
+            impact.Play();
+            deathSound.Play();            
 
             gameOver = true;
             Debug.Log("Game Over!");
@@ -67,4 +94,5 @@ public class PlayerController : MonoBehaviour
         }        
         
     }
+
 }
